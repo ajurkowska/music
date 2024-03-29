f4c7fc6b40368b43a04dfbcc4326e95c1946ee80
ajurkowska <ang.jurkowska@gmail.com>
2024-03-29 10:59:41 +0100

    fix time format

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,15 +3,22 @@ import '../styles/Song.css';
 
 const Song = (props) => {
 	const { title, artist, album, duration, rank, image, preview } = props.result;
+
+	const formatTime = (duration) => {
+		const minutes = Math.floor(duration / 60);
+		const seconds = duration % 60;
+		return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+		}
+	
 	return (
 		<li className="list__item">
 			<img src={image} alt="title" />
 			<div className="item__info">
 				<span>{title}</span>
-				<p>{artist}</p>
-				<p>{album}</p>
-				<p>{(duration / 60).toFixed(2)}</p>
-				<p>{rank}</p>
+				<p>Artist: {artist}</p>
+				<p>Album: {album}</p>
+				<p>Duration: {formatTime(duration)}</p>
+				<p>Rank: {rank}</p>
 			</div>
 			<audio controls>
 				<source src={preview} type="audio/mpeg" />
